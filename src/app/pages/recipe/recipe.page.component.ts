33f2981a2c092a7fb46c37f749bbb919affc086e
Juan Casanova <juancasanovagago@gmail.com>
2025-01-26 17:40:59 +0100

    feat: finish recipe page templating

--- src/app/pages/recipe/recipe.page.component.ts
+++ src/app/pages/recipe/recipe.page.component.ts
@@ -1,18 +1,20 @@
-import { Component, computed } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import { ScreenSizeService } from '../../../services/screen-size.service';
 import {Editor} from 'primeng/editor';
 import {FormsModule} from '@angular/forms';
 import {Button} from 'primeng/button';
+import {CustomInputComponent} from '../../components/custom-input/custom-input.component';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.page.component.html',
   styleUrl: './recipe.page.component.scss',
   standalone: true,
-  imports: [Editor, FormsModule, Button],
+  imports: [Editor, FormsModule, Button, CustomInputComponent],
 })
 export class RecipePageComponent {
   isMobile = computed(() => this.screenSize.isMobile());
+  isEditing = signal<boolean>(false);
   recipeDescription: any;
   recipeName: any;
   constructor(private screenSize: ScreenSizeService) {}

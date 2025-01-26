f6eed9e1f1f54ca7f410c8b2b5380434299042c4
Juan Casanova <juancasanovagago@gmail.com>
2025-01-26 15:51:10 +0100

    feat: remove unused variables and method from create and add recipe page styles

--- src/app/pages/create/create.page.component.ts
+++ src/app/pages/create/create.page.component.ts
@@ -1,9 +1,9 @@
-import { Component, computed, linkedSignal, signal } from '@angular/core';
+import { Component, computed, linkedSignal } from '@angular/core';
 import { Button } from 'primeng/button';
 import { CustomInputComponent } from '../../components/custom-input/custom-input.component';
 import { Editor } from 'primeng/editor';
 import { FileUpload } from 'primeng/fileupload';
-import {FormControl, FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ScreenSizeService } from '../../../services/screen-size.service';
 import { recipeService } from '../../services/recipes.service';
 
@@ -15,23 +15,17 @@ import { recipeService } from '../../services/recipes.service';
   imports: [Button, CustomInputComponent, Editor, FileUpload, FormsModule],
 })
 export class CreatePageComponent {
-  loadedRecipe = signal<boolean>(true);
-  ingredients = signal<FormControl[]>([]);
-  ingredientInput = signal<string>('');
   isMobile = computed(() => this.screenSize.isMobile());
   generatedRecipe = computed(() => this.recipeService.generatedRecipe());
-  recipeName = linkedSignal(() => this.recipeService.generatedRecipe()?.name ?? '');
-  recipeDescription = linkedSignal(() => this.recipeService.generatedRecipe()?.recipe ?? '');
+  recipeName = linkedSignal(
+    () => this.recipeService.generatedRecipe()?.name ?? '',
+  );
+  recipeDescription = linkedSignal(
+    () => this.recipeService.generatedRecipe()?.recipe ?? '',
+  );
 
-
-  constructor(private screenSize: ScreenSizeService, private recipeService: recipeService) {
-  }
-
-  addIngredient(): void {
-    const newIngredient = new FormControl(this.ingredientInput(), {
-      nonNullable: true,
-    });
-    this.ingredients.set([...this.ingredients(), newIngredient]);
-    this.ingredientInput.set('');
-  }
+  constructor(
+    private screenSize: ScreenSizeService,
+    private recipeService: recipeService,
+  ) {}
 }
--- src/app/pages/recipe/recipe.page.component.ts
+++ src/app/pages/recipe/recipe.page.component.ts
@@ -1,13 +1,19 @@
 import { Component, computed } from '@angular/core';
 import { ScreenSizeService } from '../../../services/screen-size.service';
+import {Editor} from 'primeng/editor';
+import {FormsModule} from '@angular/forms';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.page.component.html',
   styleUrl: './recipe.page.component.scss',
   standalone: true,
+  imports: [Editor, FormsModule, Button],
 })
 export class RecipePageComponent {
   isMobile = computed(() => this.screenSize.isMobile());
+  recipeDescription: any;
+  recipeName: any;
   constructor(private screenSize: ScreenSizeService) {}
 }

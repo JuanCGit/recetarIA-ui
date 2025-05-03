4ac0520ef6d5faf258a253b8e89230e0afa2e371
JuanCGit <juancasanovagago@gmail.com>
2025-05-03 20:06:02 +0200

    fix: inject router within injection context

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,26 +1,24 @@
-import {catchError, map, switchMap, take, throwError} from 'rxjs';
-import {HttpInterceptorFn} from '@angular/common/http';
-import {inject} from '@angular/core';
-import {TokenService} from '../services/auth/token.service';
-import {Router} from '@angular/router';
+import { catchError, map, switchMap, take, throwError } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { TokenService } from '../services/auth/token.service';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenService);
+  const router = inject(Router);
 
   return tokenService.token$.pipe(
     take(1),
     map((token) => {
-      if (token) {
-        return req.clone({
-          headers: req.headers.set('Authorization', `Bearer ${token}`),
-        });
-      }
-      return req;
+      if (!token) return req;
+      return req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`),
+      });
     }),
     switchMap((request) => next(request)),
     catchError((error) => {
       if (error.status === 401) {
-        const router = inject(Router);
         localStorage.removeItem('token');
         router.navigate(['/login']);
       }
